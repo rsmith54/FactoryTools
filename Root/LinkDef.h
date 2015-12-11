08ba88c52694f5cc934b2250ed063c3cd75f7ff1
Russell Smith <rsmith@cern.ch>
2015-12-11 19:12:42 +0100

    cmt stuff

---
+++ Root/LinkDef.h
@@ -0,0 +1,20 @@
+#include <DibosonRJ/RJigsawCalculator.h>
+
+#include <DibosonRJ/CalibrateST.h>
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclass;
+
+#endif
+
+#ifdef __CINT__
+#pragma link C++ class CalibrateST+;
+#endif
+
+#ifdef __CINT__
+#pragma link C++ class RJigsawCalculator+;
+#endif
