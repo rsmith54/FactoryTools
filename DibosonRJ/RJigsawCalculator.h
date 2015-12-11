71439d83f860da551542f4ea5c63fce9ce3c7b03
Russell Smith <rsmith@cern.ch>
2015-12-11 19:13:29 +0100

    RJigsaw calculator alg

---
+++ DibosonRJ/RJigsawCalculator.h
@@ -0,0 +1,42 @@
+#ifndef DibosonRJ_RJigsawCalculator_H
+#define DibosonRJ_RJigsawCalculator_H
+
+#include <EventLoop/Algorithm.h>
+
+class RJigsawCalculator : public EL::Algorithm
+{
+  // put your configuration variables here as public variables.
+  // that way they can be set directly from CINT and python.
+public:
+  // float cutValue;
+
+
+
+  // variables that don't get filled at submission time should be
+  // protected from being send from the submission node to the worker
+  // node (done by the //!)
+public:
+  // Tree *myTree; //!
+  // TH1 *myHist; //!
+
+
+
+  // this is a standard constructor
+  RJigsawCalculator ();
+
+  // these are the functions inherited from Algorithm
+  virtual EL::StatusCode setupJob (EL::Job& job);
+  virtual EL::StatusCode fileExecute ();
+  virtual EL::StatusCode histInitialize ();
+  virtual EL::StatusCode changeInput (bool firstFile);
+  virtual EL::StatusCode initialize ();
+  virtual EL::StatusCode execute ();
+  virtual EL::StatusCode postExecute ();
+  virtual EL::StatusCode finalize ();
+  virtual EL::StatusCode histFinalize ();
+
+  // this is needed to distribute the algorithm to the workers
+  ClassDef(RJigsawCalculator, 1);
+};
+
+#endif
