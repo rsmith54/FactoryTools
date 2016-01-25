eebdac8dc5b6c6932829f5a68df75184eda2c776
lawrenceleejr <lawrenceleejr@gmail.com>
2016-01-25 13:49:12 +0100

    fixed inconsequential typo in run script

--- util/run_lvlv.py
+++ util/run_lvlv.py
@@ -80,7 +80,7 @@ algsToRun = collections.OrderedDict()
 
 algsToRun["basicEventSelection"]       = ROOT.BasicEventSelection()
 algsToRun["basicEventSelection"].setConfig("$ROOTCOREBIN/data/RJigsawTools/baseEvent.config")
-algsToRun["mcEventInfo"]               = ROOT.MCEventVeto()
+algsToRun["mcEventVeto"]               = ROOT.MCEventVeto()
 
 algsToRun["calibrateST"]               = ROOT.CalibrateST()
 algsToRun["preselectDileptonicWW"]     = ROOT.PreselectDileptonicWWEvents()
