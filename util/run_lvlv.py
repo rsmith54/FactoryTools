c403284e60c11b1e360980439f83e9fcf3d39c6b
Russell Smith <rsmith@cern.ch>
2016-01-26 18:31:15 +0100

    systematic loop setup in run script

--- util/run_lvlv.py
+++ util/run_lvlv.py
@@ -21,7 +21,6 @@ parser.add_option("--driver", help      = "select where to run", choices=("direc
 parser.add_option('--doOverwrite', help = "Overwrite submit dir if it already exists",action="store_true", default=False)
 parser.add_option('--nevents', help     = "Run n events ", default = -1 )
 parser.add_option('--verbosity', help   = "Run all algs at the selected verbosity.",choices=("info", "warning","error", "debug", "verbose"), default="error")
-
 #parser.add_option("--whichAnalysis", help="select analysis", choices=("noCut", "Zmumu" , "Zee", "Wenu","NONE"), default="NONE")
 #parser.add_option("--errorLevel", help="select error level", choices=("VERBOSE","DEBUG","WARNING","ERROR"), default="WARNING")
 
@@ -46,9 +45,18 @@ def setVerbosity ( alg , levelString ) :
         quiet_exit()
     alg.setMsgLevel(level)
 
+def setupST() :
+    susyTools = ROOT.ST.SUSYObjDef_xAOD("getlist")
+    #we need to ensure these settings are the same as those used by the CalibrateST alg
+    susyTools.setProperty("METDoTrkSyst", True );
+    susyTools.setProperty("METDoCaloSyst", False );
+
+    logging.info("initializing SUSYTools")
+    susyTools.initialize()
+    return susyTools
+
 
 ROOT.gROOT.Macro( '$ROOTCOREDIR/scripts/load_packages.C' )
-
 # Initialize the xAOD infrastructure
 #ROOT.xAOD.Init()
 
@@ -86,38 +94,43 @@ job.useXAOD()
 
 logging.info("creating algorithms")
 
-outputFilename = "trees"
-output = ROOT.EL.OutputStream(outputFilename);
+susyTools = setupST()
 
 import collections
 algsToRun = collections.OrderedDict()
 
+outputFilename = "trees"
+output = ROOT.EL.OutputStream(outputFilename);
 
 algsToRun["basicEventSelection"]       = ROOT.BasicEventSelection()
 #algsToRun["basicEventSelection"].DerivationName = 'HIGG2D1'
 algsToRun["basicEventSelection"].m_useMetaData  = False
-#algsToRun["basicEventSelection"].setConfig("$ROOTCOREBIN/data/RJigsawTools/baseEvent.config")
 algsToRun["mcEventVeto"]               = ROOT.MCEventVeto()
 
-algsToRun["calibrateST"]               = ROOT.CalibrateST()
-algsToRun["preselectDileptonicWW"]     = ROOT.PreselectDileptonicWWEvents()
-algsToRun["selectDileptonicWW"]        = ROOT.SelectDileptonicWWEvents()
-algsToRun["postselectDileptonicWW"]    = ROOT.PostselectDileptonicWWEvents()
+for syst in susyTools.getSystInfoList() :
+    logging.info('applying systematic variation : ' + syst.systset.name() )
+
+    algsToRun["calibrateST_" + syst.systset.name() ]               = ROOT.CalibrateST()
+    algsToRun["calibrateST_" + syst.systset.name() ].systName      = syst.systset.name()
+
+    algsToRun["preselectDileptonicWW_" + syst.systset.name() ]     = ROOT.PreselectDileptonicWWEvents()
+    algsToRun["selectDileptonicWW_" + syst.systset.name() ]        = ROOT.SelectDileptonicWWEvents()
+    algsToRun["postselectDileptonicWW_" + syst.systset.name() ]    = ROOT.PostselectDileptonicWWEvents()
 
 #todo move the enums to a separate file since they are shared by multiple algs
-algsToRun["calculateRJigsawVariables"]                = ROOT.CalculateRJigsawVariables()
-algsToRun["calculateRJigsawVariables"].calculatorName = ROOT.CalculateRJigsawVariables.lvlvCalculator
-algsToRun["calculateRegionVars"]                      = ROOT.CalculateRegionVars()
-algsToRun["calculateRegionVars"].calculatorName       = ROOT.CalculateRegionVars.lvlvCalculator
+    algsToRun["calculateRJigsawVariables_" + syst.systset.name() ]                = ROOT.CalculateRJigsawVariables()
+    algsToRun["calculateRJigsawVariables_" + syst.systset.name() ].calculatorName = ROOT.CalculateRJigsawVariables.lvlvCalculator
+    algsToRun["calculateRegionVars_" + syst.systset.name() ]                      = ROOT.CalculateRegionVars()
+    algsToRun["calculateRegionVars_" + syst.systset.name() ].calculatorName       = ROOT.CalculateRegionVars.lvlvCalculator
 
-for regionName in ["SR","CR1L","CR0L"]:
-    tmpWriteOutputNtuple                       = ROOT.WriteOutputNtuple()
-    tmpWriteOutputNtuple.outputName            = outputFilename
-    tmpWriteOutputNtuple.regionName            = regionName
-    algsToRun["writeOutputNtuple_"+regionName] = tmpWriteOutputNtuple
-
+    for regionName in ["SR","CR1L","CR0L"]:
+        tmpWriteOutputNtuple                       = ROOT.WriteOutputNtuple()
+        tmpWriteOutputNtuple.outputName            = outputFilename
+        tmpWriteOutputNtuple.regionName            = regionName + '_' + syst.systset.name()
+        algsToRun["writeOutputNtuple_"+regionName + syst.systset.name()] = tmpWriteOutputNtuple
 
 job.outputAdd(output);
+
 for name,alg in algsToRun.iteritems() :
     setVerbosity(alg , options.verbosity)
     logging.info("adding " + name + " to algs" )
