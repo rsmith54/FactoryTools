8ee254906177e9fd78b437a871547d2f9c35901a
lawrenceleejr <lawrenceleejr@gmail.com>
2016-02-22 02:16:27 +0100

    adding opt parser to merging script and just making it generally a little better

--- util/discoverInput.py
+++ util/discoverInput.py
@@ -29,22 +29,42 @@ def addTags(sh_all):
 		if "physics_" in short_name:
 			sample.addTag("data")
 
-		if "GG_direct" in short_name:
-			sample.addTag("signal")
-			sample.addTag("gg_direct")
-		if "SS_direct" in short_name:
-			sample.addTag("signal")
-			sample.addTag("ss_direct")
+		#if "GG_direct" in short_name:
+		#	sample.addTag("signal")
+		#	sample.addTag("gg_direct")
+		#if "SS_direct" in short_name:
+		#	sample.addTag("signal")
+		#	sample.addTag("ss_direct")
 
-		if "ttbar" in short_name:
+
+		if "SM_GG_N2" in short_name:
+			sample.addTag("signal")
+			sample.addTag("gluino")
+
+		if "GG_onestepCC" in short_name:
+			sample.addTag("signal")
+			sample.addTag("gg_onestepcc")
+
+		if "SS_onestepCC" in short_name:
+			sample.addTag("signal")
+			sample.addTag("ss_onestepcc")
+
+
+		if "PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad" in short_name:
 			sample.addTag("top")
 			sample.addTag("ttbar")
 		if "Wt_inclusive" in short_name:
 			sample.addTag("top")
 			sample.addTag("singletop")
-		if "PowHPEvG_singletop" in short_name:
+		if "PowhegPythiaEvtGen_P2012_singletop_" in short_name:
 			sample.addTag("top")
 			sample.addTag("singletop")
+		if "PowhegPythiaEvtGen_P2012_SingleTopSchan_noAllHad_" in short_name:
+			sample.addTag("top")
+			sample.addTag("singletop")
+		if "MadGraphPythia8EvtGen_A14NNPDF23LO_tt" in short_name or "MadGraphPythia8EvtGen_A14NNPDF23_ttbar" in short_name:
+			sample.addTag("top")
+			sample.addTag("ttv")
 
 		if "jetjet" in short_name:
 			sample.addTag("qcd")
@@ -52,15 +72,22 @@ def addTags(sh_all):
 		if "1Gam" in short_name:
 			sample.addTag("gamma")
 
-		if "PowHP8EvG_W" in short_name:
+		if "Sherpa_CT10_W" in short_name:
 			sample.addTag("wjets")
-		if "PowHP8EvG_Z" in short_name:
+		if "Sherpa_CT10_Z" in short_name:
 			sample.addTag("zjets")
-		if "Znunu" in short_name:
-			sample.addTag("zjets")
-		if "Sherpa_Wqq" in short_name:
-			sample.addTag("zjets")
-		if "_Zmumu_" in short_name:
-			sample.addTag("zjets")
-		if "ggH" in short_name:
-			sample.addTag("signal")
+		# if "Znunu" in short_name:
+		# 	sample.addTag("zjets")
+		# if "Sherpa_Wqq" in short_name:
+		# 	sample.addTag("zjets")
+		# if "_Zmumu_" in short_name:
+		# 	sample.addTag("zjets")
+		# if "ggH" in short_name:
+		# 	sample.addTag("signal")
+
+
+		if "PowhegPy8EG_CT10nloME_AZNLOCTEQ6L1_" in short_name and any(x in short_name for x in ["WW","ZZ","WZ"]):
+			sample.addTag("diboson")
+
+		if any(x in short_name for x in ["Sherpa_CT10_munugamma","Sherpa_CT10_nunugamma","Sherpa_CT10_ll","Sherpa_CT10_gg"]):
+			sample.addTag("electroweak")
