e90d768759870b16cb4247455d390eb54e3fd8b3
Russell Smith <rsmith@cern.ch>
2016-03-22 11:17:31 +0100

    adding a parent function to do all calculations we want for every analysis

---
+++ Root/RegionVarCalculator.cxx
@@ -0,0 +1,36 @@
+#include "EventLoop/StatusCode.h"
+#include "EventLoop/Worker.h"
+
+#include "xAODRootAccess/TEvent.h"
+#include "xAODEventInfo/EventInfo.h"
+
+#include "RJigsawTools/RegionVarCalculator.h"
+#include "RJigsawTools/strongErrorCheck.h"
+
+
+// this is needed to distribute the algorithm to the workers
+ClassImp(RegionVarCalculator)
+
+EL::StatusCode RegionVarCalculator::doGeneralCalculations(std::map<std::string, double>& RegionVars,
+							  std::map<std::string, std::vector<double> > & VecRegionVars)
+{
+  xAOD::TEvent * event = m_worker->xaodEvent();
+  // Get relevant info from the EventInfo object
+
+  const xAOD::EventInfo* eventInfo = nullptr;
+  STRONG_CHECK(event->retrieve( eventInfo, "EventInfo"));
+
+  RegionVars["runNumber"]   = eventInfo->runNumber();
+  RegionVars["lumiBlock"]   = eventInfo->lumiBlock();
+  RegionVars["bcid"]        = eventInfo->bcid();
+  RegionVars["eventNumber"] = eventInfo->eventNumber();
+  RegionVars["mcChannelNumber"] = eventInfo->mcChannelNumber();
+  RegionVars["actualInteractionsPerCrossing"] = eventInfo->actualInteractionsPerCrossing();
+  RegionVars["averageInteractionsPerCrossing"] = eventInfo->averageInteractionsPerCrossing();
+
+  RegionVars["mcEventWeight"] = eventInfo->auxdecor< float >("mcEventWeight");
+  RegionVars["pileupWeight"]  = eventInfo->auxdecor< float >("PileupWeight");
+
+  return EL::StatusCode::SUCCESS;
+}
+
