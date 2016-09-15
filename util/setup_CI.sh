ae98840ec398d285b34b528e2fc054d4b6175631
lawrenceleejr <lawrenceleejr@gmail.com>
2016-09-16 01:48:32 +0200

    list releases in CI

--- util/setup_CI.sh
+++ util/setup_CI.sh
@@ -4,5 +4,6 @@ ls -l /cvmfs/atlas.cern.ch/repo
 pwd
 source ~/.bashrc  || echo ignore alrb
 lsetup root || echo ignore alrb
+rcSetup -r || echo ignore alrb
 rcSetup Base,2.4.X,rel_0 || echo ignore alrb
   # - rc find_packages
