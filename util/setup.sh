494d05be96b2a5e375df391391ea56b926a05669
Giordon Stark <kratsg@gmail.com>
2016-09-15 23:45:23 +0200

    update setup script

--- util/setup.sh
+++ util/setup.sh
@@ -1,10 +1,6 @@
-export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
-setupATLAS # or equivalent on your machine
-localSetupDQ2Client
-localSetupFAX
-localSetupPyAMI
-localSetupROOT
+#export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+#alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
+setupATLAS || true
+lsetup fax pyami root dq2 'rcSetup Base,2.4.X,rel_0'
 #rcSetup Base,2.4.18
-rcSetup Base,2.4.X,rel_0
-voms-proxy-init -voms atlas
+#voms-proxy-init -voms atlas
