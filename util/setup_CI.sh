46c1d1c742bc484c2b7fdc36ce2a337f1e895bd8
lawrenceleejr <lawrenceleejr@gmail.com>
2016-09-17 10:58:55 +0200

    trying to edit ssh config for kerberos svn access

--- util/setup_CI.sh
+++ util/setup_CI.sh
@@ -8,5 +8,20 @@ rcSetup -r || echo ignore alrb
 #rcSetup Base,2.4.X,rel_0 || echo ignore alrb
 rcSetup Base,2.4.18 || echo ignore alrb
 
+echo "Host svn.cern.ch
+   User ${serviceUser}
+   GSSAPIAuthentication yes
+   GSSAPIDelegateCredentials yes
+   Protocol 2
+   ForwardX11 no
+   IdentityFile ~/.ssh/id_rsa
+Host isscvs.cern.ch
+   User ${serviceUser}    
+   GSSAPIAuthentication yes
+   GSSAPIDelegateCredentials yes
+   Protocol 2
+   ForwardX11 no
+   IdentityFile ~/.ssh/id_rsa" > ~/.ssh/config
+
 echo "${servicePass}" | kinit ${serviceUser}@CERN.CH
 klist
