e7c357b467c695cf0173e11abdd2dc37d4046adc
lawrenceleejr <lawrenceleejr@gmail.com>
2016-09-18 15:26:02 +0200

    trying with keys

--- util/setup_CI.sh
+++ util/setup_CI.sh
@@ -8,13 +8,6 @@ rcSetup -r || echo ignore alrb
 #rcSetup Base,2.4.X,rel_0 || echo ignore alrb
 rcSetup Base,2.4.18 || echo ignore alrb
 
-# echo "Host svn.cern.ch
-#    User ${serviceUser}
-#    GSSAPIAuthentication yes
-#    GSSAPIDelegateCredentials yes
-#    Protocol 2
-#    IdentityFile ~/.ssh/id_rsa" | tee ~/.ssh/config
-
 echo "${servicePass}" | kinit ${serviceUser}@CERN.CH
 klist
 
