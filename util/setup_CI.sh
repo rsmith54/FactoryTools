42615920ae6df62c84d45798de0a7d76f495242e
lawrenceleejr <lawrenceleejr@gmail.com>
2016-09-18 14:42:24 +0200

    see output of echo..2

--- util/setup_CI.sh
+++ util/setup_CI.sh
@@ -8,13 +8,12 @@ rcSetup -r || echo ignore alrb
 #rcSetup Base,2.4.X,rel_0 || echo ignore alrb
 rcSetup Base,2.4.18 || echo ignore alrb
 
-echo "Host svn.cern.ch
-   User ${serviceUser}
-   GSSAPIAuthentication yes
-   GSSAPIDelegateCredentials yes
-   Protocol 2
-   ForwardX11 no
-   IdentityFile ~/.ssh/id_rsa" | tee ~/.ssh/config
+# echo "Host svn.cern.ch
+#    User ${serviceUser}
+#    GSSAPIAuthentication yes
+#    GSSAPIDelegateCredentials yes
+#    Protocol 2
+#    IdentityFile ~/.ssh/id_rsa" | tee ~/.ssh/config
 
 echo "${servicePass}" | kinit ${serviceUser}@CERN.CH
 klist
