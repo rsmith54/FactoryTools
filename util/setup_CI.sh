bd00e214f5fe57f3a593472325fe50e6c3705500
lawrenceleejr <lawrenceleejr@gmail.com>
2016-09-18 17:49:51 +0200

    undoing ssh config change..

--- util/setup_CI.sh
+++ util/setup_CI.sh
@@ -11,12 +11,12 @@ rcSetup Base,2.4.18 || echo ignore alrb
 echo "${servicePass}" | kinit ${serviceUser}@CERN.CH
 klist
 
-echo "Host svn.cern.ch
-   User atlzerol
-   GSSAPIAuthentication yes
-   GSSAPIDelegateCredentials yes
-   Protocol 2
-   ForwardX11 no
-   IdentityFile ~/.ssh/id_rsa" > ~/.ssh/config
+# echo "Host svn.cern.ch
+#    User atlzerol
+#    GSSAPIAuthentication yes
+#    GSSAPIDelegateCredentials yes
+#    Protocol 2
+#    ForwardX11 no
+#    IdentityFile ~/.ssh/id_rsa" > ~/.ssh/config
 
 # ssh -t -t atlzerol@svn.cern.ch
